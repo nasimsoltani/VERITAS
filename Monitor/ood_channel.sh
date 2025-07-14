8c0902e4b649750d3d2231228933d2f97d59d202
nasimsoltani <nasimsoltani67@gmail.com>
2025-07-14 13:43:12 -0500

    paths changed

--- Monitor/ood_channel.sh
+++ Monitor/ood_channel.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #---------------------------------------------------
-python -u /home/ns38942/DeepRx/deep-rx-sim-env/Monitor/main.py \
+python -u /home/ns38942/VERITAS/Monitor/main.py \
 --gpu_id $1 \
 --channel_param 'Channel_Profile' \
---train_dataset_path '../../MonitorDatasetsPkl/training_rx_pilot_for_10_grids_channelprofiles.pkl' \
---test_dataset_path '../../MonitorDatasetsPkl/test_rx_pilot_for_10_grids_channelprofiles.pkl' \
+--train_dataset_path '../MonitorDatasetsPkl/training_rx_pilot_for_10_grids_channelprofiles.pkl' \
+--test_dataset_path '../MonitorDatasetsPkl/test_rx_pilot_for_10_grids_channelprofiles.pkl' \
 --train \
 --test \
---weight_path '/home/ns38942/DeepRx/deep-rx-sim-env/Monitor/results/weights_Channel_Profile.pt' \
+--weight_path '/home/ns38942/VERITAS/Monitor/results/weights_Channel_Profile.pt' \
 > ./results/log-Channel_Profile.out \
 2> ./results/log-Channel_Profile.err
--- Monitor/ood_delay.sh
+++ Monitor/ood_delay.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #---------------------------------------------------
-python -u /home/ns38942/DeepRx/deep-rx-sim-env/Monitor/main.py \
+python -u /home/ns38942/VERITAS/Monitor/main.py \
 --gpu_id $1 \
 --channel_param 'Delay_Spread' \
---train_dataset_path '../../MonitorDatasetsPkl/training_rx_pilot_for_10_grids_delay.pkl' \
---test_dataset_path '../../MonitorDatasetsPkl/test_rx_pilot_for_10_grids_delay.pkl' \
+--train_dataset_path '../MonitorDatasetsPkl/training_rx_pilot_for_10_grids_delay.pkl' \
+--test_dataset_path '../MonitorDatasetsPkl/test_rx_pilot_for_10_grids_delay.pkl' \
 --train \
 --test \
---weight_path '/home/ns38942/DeepRx/deep-rx-sim-env/Monitor/results/weights_Delay_Spread.pt' \
+--weight_path '/home/ns38942/VERITAS/Monitor/results/weights_Delay_Spread.pt' \
 > ./results/log-Delay_Spread.out \
 2> ./results/log-Delay_Spread.err
